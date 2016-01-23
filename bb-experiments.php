c8f9a99dbdc04b96fc03bb87268d8462372c1135
Brent Jett <brentjett@gmail.com>
2016-01-23 12:30:52 -0600

    update mini map and ui themes

--- bb-experiments.php
+++ bb-experiments.php
@@ -9,33 +9,46 @@ Author URI: http://brentjett.design
 
 define( 'BB_EXPERIMENTS_DIR', plugin_dir_path( __FILE__ ) );
 
-$experiments = array(
-    array(
-        'key' => 'bb-minimap',
-        'label' => 'Beaver Builder Minimap',
-        'include' => BB_EXPERIMENTS_DIR . '/bb-minimap/bb-minimap.php'
-    ),
-    array(
-        'key' => 'bb-store-kit',
-        'label' => 'Beaver Builder Store Kit',
-        'include' => BB_EXPERIMENTS_DIR . '/bb-store-kit/bb-store-kit.php'
-    ),
-    array(
-        'key' => 'bb-tutorials',
-        'label' => 'Beaver Builder Tutorials Plugin Base',
-        'include' => BB_EXPERIMENTS_DIR . '/bb-tutorials/bb-tutorials.php'
-    ),
-    array(
-        'key' => 'bb-ui-themes',
-        'label' => 'Beaver Builder UI Themes',
-        'include' => BB_EXPERIMENTS_DIR . '/bb-ui-themes/bb-ui-themes.php'
-    )
-);
-
 // Optionally Include Projects
-if (!empty($experiments)) {
-    foreach($experiments as $proj) {
-        require_once $proj['include'];
+function brj_include_experiments() {
+    $experiments = array(
+        array(
+            'key' => 'bb-minimap',
+            'label' => 'Beaver Builder Minimap',
+            'include' => BB_EXPERIMENTS_DIR . '/bb-minimap/bb-minimap.php'
+        ),
+        array(
+            'key' => 'bb-store-kit',
+            'label' => 'Beaver Builder Store Kit',
+            'include' => BB_EXPERIMENTS_DIR . '/bb-store-kit/bb-store-kit.php'
+        ),
+        array(
+            'key' => 'bb-tutorials',
+            'label' => 'Beaver Builder Tutorials Plugin Base',
+            'include' => BB_EXPERIMENTS_DIR . '/bb-tutorials/bb-tutorials.php'
+        ),
+        array(
+            'key' => 'bb-ui-themes',
+            'label' => 'Beaver Builder UI Themes',
+            'include' => BB_EXPERIMENTS_DIR . '/bb-ui-themes/bb-ui-themes.php'
+        )
+    );
+    
+    if (!empty($experiments)) {
+        foreach($experiments as $proj) {
+            if ( apply_filters('brj/include_experiment', true, $proj['key']) ) {
+                require_once $proj['include'];
+            }
+        }
     }
 }
+add_action('plugins_loaded', 'brj_include_experiments');
+
+function brj_check_include_experiment($bool = true, $key) {
+
+    // @todo: Add UI Check Here
+
+    return $bool;
+}
+add_filter('brj/include_experiment', 'brj_check_include_experiment', 10, 2);
 ?>
--- bb-minimap/bb-minimap.php
+++ bb-minimap/bb-minimap.php
@@ -6,7 +6,7 @@ Author: Brent Jett
 Description: A small script to allow you to see the parts of your layout at a glance.
 */
 function brj_enqueue_minimap() {
-    if (is_user_logged_in()) {
+    if (is_user_logged_in() && class_exists('FLBuilderModel') && FLBuilderModel::is_builder_active() ) {
         wp_enqueue_script('bb-minimap', plugins_url('bb-minimap.js', __FILE__), array('jquery', 'wp-util'));
         wp_enqueue_style('bb-minimap', plugins_url('css/bb-minimap.css', __FILE__));
     }
